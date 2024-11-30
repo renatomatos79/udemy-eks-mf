ab947fbc9f229ffc2a168ca35e4ee7a34e1fc8bc
renato matos <renato.matos79@gmail.com>
2024-11-30 12:56:25 +0000

    adding eks file

--- backend/UsersApi/Program.cs
+++ backend/UsersApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using UsersApi.Helpers;
 using UsersApi.Model.Settings;
 using UsersApi.Repository;
@@ -9,7 +10,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Users API", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 
 // Add services to the container.
 builder.Services.AddSingleton<IDbSettngs>(_ =>
@@ -32,14 +61,8 @@ builder.Services.AddScoped(sp =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- backend/UsersApi/readme.txt
+++ backend/UsersApi/readme.txt
@@ -2,9 +2,9 @@
 https://www.mongodb.com/resources/products/compatibilities/docker
 
 => criando o container mongodb com usuario admin e senha m0ng0d4t4 e disco mapeado para C:\data\mongodb
-docker run --name mongodb2 -d -p 27017:27017 -v C:\data\mongodb2:/data/db -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=m0ng0d4t4 mongodb/mongodb-community-server:6.0-ubi8
+docker run --name mongodb3 -d -p 27019:27017 -v C:\data\mongodb3:/data/db -e MONGODB_INITDB_ROOT_USERNAME=admin -e MONGODB_INITDB_ROOT_PASSWORD=m0ng0d4t4 mongodb/mongodb-community-server:6.0-ubi8
 
-=> acessando o container do visual studio
+=> connection string para acessar o container do visual studio
 mongodb://admin:m0ng0d4t4@localhost:27017/
 
 => building docker image
@@ -12,7 +12,7 @@ docker build -t users-api:8.0.1 .
 
 => running container
 => DONT FORGET TO BE LOGGED INTO DOCKER DESKTOP
-docker run -d --name users-api -p 8085:8080 -e APP_MONGODB_CS=mongodb://admin:m0ng0d4t4@192.168.1.254:27018 users-api:8.0.1
+docker run -d --name users-api -p 8085:8080 -e APP_MONGODB_CS=mongodb://admin:m0ng0d4t4@192.168.1.254:27019 users-api:8.0.1
 
 => accessing container logs
 docker container logs users-api
